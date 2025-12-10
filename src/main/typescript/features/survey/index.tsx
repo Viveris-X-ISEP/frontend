cb6df483332f9ae0fc9645752040bfcd86c4a714
Joaquim-Keloglanian <joaquim.keloglanian@gmail.com>
2025-12-10 15:45:30 +0100

    feat(survey): add feature barrel exports.
    - Export SurveyIntroScreen and SurveyQuestionsScreen components
    - Export AnswerOption component
    - Re-export hooks and types from feature index
    
    Refs: #feature/footprint-quizz

---
+++ src/main/typescript/features/survey/index.tsx
@@ -0,0 +1,5 @@
+export { default as SurveyIntroScreen } from "./components/survey-intro.screen";
+export { default as SurveyQuestionsScreen } from "./components/survey-questions.screen";
+export { default as AnswerOption } from "./components/answer-option.component";
+export * from "./hooks";
+export * from "./types";
